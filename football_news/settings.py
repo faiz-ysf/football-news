5fd76daa17688acaadacb65c33658940ee94ec89
Faiz Yusuf <faiswusuf@gmail.com>
2025-09-10 16:16:49 +0700

    perbaiki bug pada csrf

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "faiz-yusuf-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<https://faiz-yusuf-footballnews.pbp.cs.ui.ac.id>"
+    "https://faiz-yusuf-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
