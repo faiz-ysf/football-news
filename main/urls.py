707e5f32d874a828a125d8344ae2a0adeb4d18e9
Faiz Yusuf <faiswusuf@gmail.com>
2025-09-24 14:58:01 +0700

    selesai tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from main.views import(show_main, 
 	                   create_news, show_news, show_xml,
 				       show_json, show_xml_by_id, show_json_by_id, 
-					   register, login_user, logout_user )
+					   register, login_user, logout_user, 
+					   edit_news, delete_news )
 
 app_name = 'main'
 
@@ -14,6 +15,10 @@ urlpatterns = [
     path('', show_main, name='show_main'),
 	path('create-news/', create_news, name='create_news'),
     path('news/<str:id>/', show_news, name='show_news'),
+
+	path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+	path('news/<uuid:id>/delete', delete_news, name='delete_news'),
+
 
 	path('xml/', show_xml, name='show_xml'),
 	path('json/', show_json, name='show_json'),
